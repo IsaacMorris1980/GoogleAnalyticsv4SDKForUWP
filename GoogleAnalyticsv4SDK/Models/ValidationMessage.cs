afd98a35e427785bc8429f953e75a7e96275b3bc
Isaac Morris <morris.isaac@gmail.com>
2023-06-27 22:00:35 -0500

    starting but not ready

---
+++ GoogleAnalyticsv4SDK/Models/ValidationMessage.cs
@@ -0,0 +1,11 @@
+﻿using GoogleAnalyticsv4SDK.Validation;
+
+namespace GoogleAnalyticsv4SDK.Models
+{
+    internal class ValidationMessage
+    {
+        private string fieldPath = string.Empty;
+        private string description = string.Empty;
+        private ValidationCode validationCode;
+    }
+}
---
+++ GoogleAnalyticsv4SDK/Validation/DebugEventCalls.cs
@@ -0,0 +1,15 @@
+﻿using GoogleAnalyticsv4SDK.Models;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GoogleAnalyticsv4SDK
+{
+    public class DebugEventCalls
+    {
+        public async Task<List<ValidationMessage>> SendEvent(Event e)
+        {
+            return null;
+        }
+    }
+}
